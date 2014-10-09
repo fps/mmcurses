c57d840dfc83ba1ceaebb5c8d62963a50ec01f4c
Florian Paul Schmidt <mista.tapas@gmx.net>
2014-10-09 12:22:24 +0200

    move implementation to application.cc

--- src/mmcurses/application.h
+++ src/mmcurses/application.h
@@ -25,51 +25,27 @@ namespace mmcurses
         /**
             The constructor initializes the screen and sets up some useful defaults.
         */
-        application() :
-            m_done(false),
-            m_rc(0),
-            m_width(0),
-            m_height(0),
-            m_invalidated(false)
-        {
-            SCREEN *s = newterm(getenv("TERM"), stdout, stdin);
-            if (s == nullptr) { /* throw */ }
-            keypad(stdscr, TRUE);
-            raw();
-            noecho();
-            halfdelay(1);
-        }
+        application();
 
         /**
             Restores the terminal to the state before the constructor was called.
         */
-        virtual ~application()
-        {
-            endwin();
-        }
+        virtual ~application();
 
         /**
             Reimplement this if you want to react to terminal size changes. The default implementation just calls invalidate().
         */
-        virtual void size_changed(unsigned width, unsigned height)
-        {
-            invalidate();
-        }
+        virtual void size_changed(unsigned width, unsigned height);
    
         /**
             Causes repaint() to be called from the main loop. Refreshes the screen afterwards.
         */
-        virtual void invalidate()
-        {
-            m_invalidated = true;
-        }
+        virtual void invalidate();
    
         /**
             Put your drawing code here. The default implementation does nothing.
         */
-        virtual void repaint(unsigned width, unsigned height)
-        {
-        }
+        virtual void repaint(unsigned width, unsigned height);
 
         /**
             Reimplement this if you want to react to key presses. Check the ncurses documentation for the possible key codes k.
@@ -78,57 +54,17 @@ namespace mmcurses
             
             The default implementation just calls quit(0).
         */
-        virtual void key_pressed(int k)
-        {
-            quit(0);
-        }
-
+        virtual void key_pressed(int k);
+        
         /**
             Call this function to make the application exit at a point in the future.
         */
-        virtual void quit(int rc)
-        {
-            m_rc = rc;
-            m_done = true;
-        }
+        virtual void quit(int rc);
 
         /**
             The entry point into the application's main loop. It returns the rc passed into quit().
         */
-        virtual int exec()
-        {
-            while(false == m_done)
-            {
-                int c = getch();
-               
-                int x,y;
-                getmaxyx(stdscr, y, x);
-
-                bool size_change = x != m_width || y != m_height;
-
-                m_width = x;
-                m_height = y;
-
-                if (size_change)
-                {
-                    size_changed(m_width, m_height);
-                }        
-
-                if (m_invalidated)
-                {
-                    clear();
-                    repaint(m_width, m_height);
-                    refresh();
-                    m_invalidated = false;
-                }
-                
-                if (c != ERR && c != KEY_RESIZE)
-                {
-                    key_pressed(c);
-                }
-            }
-            return m_rc;
-        }
+        virtual int exec();
     };
 }
 
